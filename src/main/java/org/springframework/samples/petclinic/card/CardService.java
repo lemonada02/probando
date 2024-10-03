2ffa639d307321f8ea258488cc440bc7eb71c19c
lemonada02 <jrlimon02@gmail.com>
2024-10-03 19:52:11 +0200

    funciona

--- src/main/java/org/springframework/samples/petclinic/card/CardService.java
+++ src/main/java/org/springframework/samples/petclinic/card/CardService.java
@@ -1,6 +1,7 @@
 package org.springframework.samples.petclinic.card;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,5 +26,20 @@ public class CardService {
 
     public void saveCard(Card card) {
         cardRepository.save(card);
+    }
+
+    public List<Card> createAllCards(){
+
+        List<Card> cartas= new ArrayList<Card>();
+        
+        for(int i=0; i<36; i++){
+            Card carta = new Card();
+            int id = i+1;
+            //ARREGLAME AWELO
+            carta.setDesign("../../carta"+id+".png");
+            cartas.add(carta);
+        }
+
+        return cartas;
     }
 }
